763bdd408eba2ba6c7157654291c10beeed81de6
Keith Perkins <keith.perkins@tlgcohort.com>
2023-11-08 13:25:05 -0700

    Lab03.2 pay/payTaxes/equals testing Employee Test w/ anonymous subclass

--- Lab03.2/src/com/javatunes/personnel/SalariedEmployee.java
+++ Lab03.2/src/com/javatunes/personnel/SalariedEmployee.java
@@ -11,48 +11,49 @@ package com.javatunes.personnel;
 import java.sql.Date;
 import java.util.Objects;
 
-public class SalariedEmployee
-extends Employee {
+public class SalariedEmployee extends Employee {
   private Double salary;
-  
+
   public SalariedEmployee() {
   }
 
   public SalariedEmployee(String name, Date hireDate) {
     super(name, hireDate);
   }
-  
+
   public SalariedEmployee(String name, Date hireDate, Double salary) {
     this(name, hireDate);
     setSalary(salary);
   }
-  
+
   @Override
-  public long pay() {
-    System.out.println(getName() + " is paid salary " + getSalary());
-    return 0;
+  public double pay() {
+    double payment = getSalary();
+    System.out.println(getName() + " is paid salary " + payment);
+    return payment;
   }
-  
+
   @Override  // interface TaxPayer
   public double payTaxes() {
     double taxes = SALARIED_TAX_RATE * getSalary();
     System.out.println(getName() + " paid taxes of " + taxes);
     return taxes;
   }
-  
+
   public Double getSalary() {
     return this.salary;
   }
+
   public void setSalary(Double salary) {
     this.salary = salary;
   }
-  
+
   // leverage super.toString() for classname, name, and hireDate
   @Override
   public String toString() {
     return super.toString() + ", salary=" + getSalary();
   }
-  
+
   // leverage super.equals() for name, hireDate equality, check salary here
   @Override
   public boolean equals(Object obj) {
@@ -60,11 +61,11 @@ extends Employee {
     if (obj instanceof SalariedEmployee) {
       SalariedEmployee other = (SalariedEmployee) obj;
       result = super.equals(obj) &&
-               Objects.equals(this.getSalary(), other.getSalary());
+              Objects.equals(this.getSalary(), other.getSalary());
     }
     return result;
   }
-  
+
   // take the hash of superclass (name, hireDate) and add salary to the mix
   @Override
   public int hashCode() {
---
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -0,0 +1,55 @@
+package com.javatunes.personnel;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.sql.Date;
+import static org.junit.Assert.*;
+
+public class EmployeeTest {
+    private Employee emp1;
+    private Employee emp2;
+
+    @Before
+    public void setUp() {
+        // emp1 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+        // emp2 = new DummyEmployee("Mary Lou", Date.valueOf("1999-09-09"));
+
+        emp1 = getEmployee();
+        emp2 = getEmployee();
+    }
+
+    private Employee getEmployee() {
+        return new Employee("Mary Lou", Date.valueOf("1999-09-09")) {
+            public double pay() { return 0; }
+            public double payTaxes() { return 0; }
+        };
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_differentName_sameHireDate() {
+        emp2.setName("James");
+        assertNotEquals(emp1, emp2);
+    }
+
+    @Test
+    public void equals_shouldReturnFalse_sameName_differentHireDate() {
+        emp2.setHireDate(Date.valueOf("2000-02-02"));
+        assertNotEquals(emp1, emp2);
+    }
+
+    @Test
+    public void equals_shouldReturnTrue_allPropertiesSame() {
+        assertEquals(emp1, emp2);
+    }
+
+    // NAMED, MEMBER-LEVEL INNER CLASSES
+    private class DummyEmployee extends Employee {
+
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        public double pay() { return 0; }
+        public double payTaxes() { return 0; }
+    }
+}
