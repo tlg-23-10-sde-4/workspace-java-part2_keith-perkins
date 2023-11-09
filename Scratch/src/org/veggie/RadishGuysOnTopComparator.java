2282183d34ff3b68afc3fed13829da1ff0b6235a
Keith Perkins <keith.perkins@tlgcohort.com>
2023-11-09 11:18:14 -0700

    sort List<Radish> via Comparator lambda

--- Scratch/src/org/veggie/RadishGuysOnTopComparator.java
+++ Scratch/src/org/veggie/RadishGuysOnTopComparator.java
@@ -2,10 +2,11 @@ package org.veggie;
 
 import java.util.Comparator;
 
+//sort key is 'guysOnTop' (int)
 public class RadishGuysOnTopComparator implements Comparator <Radish> {
 
     @Override
     public int compare(Radish radish1, Radish radish2) {
-        return Integer.compare(radish1.getGuysOnTop(), radish2.getGuysOnTop());
+        return Integer.compare(radish1.getGuysOnTop(), radish2.getGuysOnTop());     // the only part of code that really matters, rest is "dressing" to run this
     }
 }
